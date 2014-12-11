6e6e579c0b137f2dae9631ed1f0e1367668f768a
unknown <mairo.kahar@gmail.com>
2014-12-11 16:16:13 +0200

    Styling success/error messages

--- register.php
+++ register.php
@@ -8,105 +8,107 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 </head>
 <body>
-    <form action="registerAction.php" method="post" ng-app="" ng-controller="validateController" name="registerForm" class="form-horizontal" novalidate>
-        <fieldset>
-            <legend>Register</legend>
+    <div class="container">
+        <form action="registerAction.php" method="post" ng-app="" ng-controller="validateController" name="registerForm" class="form-horizontal" novalidate>
+            <fieldset>
+                <legend>Register</legend>
 
-            <!-- Name-->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="name">Name</label>
-                <div class="col-md-2">
-                    <input id="name" type="text" class="form-control input-md" name="name" ng-model="name" ng-maxlength=20 ng-pattern="/^[a-zA-ZÀ-ž -]*$/" placeholder="Name" required>
-                    <p class="help-block"><span style="color:red" ng-show="registerForm.name.$dirty && registerForm.name.$invalid"><span ng-show="registerForm.name.$error.required">Name is required.</span><span ng-show="registerForm.name.$error.pattern">Name must contain only alphabetic letters.</span><span ng-show="registerForm.name.$error.maxlength">Name too long.</span></span></p>
+                <!-- Name-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="name">Name</label>
+                    <div class="col-md-3">
+                        <input id="name" type="text" class="form-control input-md" name="name" ng-model="name" ng-maxlength=20 ng-pattern="/^[a-zA-ZÀ-ž -]*$/" placeholder="Name" required>
+                        <p class="help-block"><span style="color:red" ng-show="registerForm.name.$dirty && registerForm.name.$invalid"><span ng-show="registerForm.name.$error.required">Name is required.</span><span ng-show="registerForm.name.$error.pattern">Name must contain only alphabetic letters.</span><span ng-show="registerForm.name.$error.maxlength">Name too long.</span></span></p>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Surname-->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="surname">Surname</label>
-                <div class="col-md-2">
-                    <input id="surname" type="text" class="form-control input-md" name="surname" ng-model="surname" ng-maxlength=20 ng-pattern="/^[a-zA-ZÀ-ž -]*$/" placeholder="Surname" ng-pattern="[a-zA-Z]" required>
-                    <span class="help-block"><span style="color:red" ng-show="registerForm.surname.$dirty && registerForm.surname.$invalid"><span ng-show="registerForm.surname.$error.required">Surname is required.</span><span ng-show="registerForm.surname.$error.pattern">Surname must contain only alphabetic letters.</span><span ng-show="registerForm.surname.$error.maxlength">Surname too long.</span></span></span>
+                <!-- Surname-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="surname">Surname</label>
+                    <div class="col-md-3">
+                        <input id="surname" type="text" class="form-control input-md" name="surname" ng-model="surname" ng-maxlength=20 ng-pattern="/^[a-zA-ZÀ-ž -]*$/" placeholder="Surname" ng-pattern="[a-zA-Z]" required>
+                        <span class="help-block"><span style="color:red" ng-show="registerForm.surname.$dirty && registerForm.surname.$invalid"><span ng-show="registerForm.surname.$error.required">Surname is required.</span><span ng-show="registerForm.surname.$error.pattern">Surname must contain only alphabetic letters.</span><span ng-show="registerForm.surname.$error.maxlength">Surname too long.</span></span></span>
+                    </div>
                 </div>
-            </div>
 
-            <!-- E-mail-->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="email">Email</label>
-                <div class="col-md-2">
-                    <input id="email" type="email" class="form-control input-md" name="email" ng-model="email" ng-maxlength=30 placeholder="E-mail" required>
-                    <span class="help-block"><span style="color:red" ng-show="registerForm.email.$dirty && registerForm.email.$invalid"><span ng-show="registerForm.email.$error.required">Email is required.</span><span ng-show="registerForm.email.$error.email">Invalid email address.</span><span ng-show="registerForm.email.$error.maxlength">E-mail too long.</span></span></span>
+                <!-- E-mail-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="email">Email</label>
+                    <div class="col-md-3">
+                        <input id="email" type="email" class="form-control input-md" name="email" ng-model="email" ng-maxlength=30 placeholder="E-mail" required>
+                        <span class="help-block"><span style="color:red" ng-show="registerForm.email.$dirty && registerForm.email.$invalid"><span ng-show="registerForm.email.$error.required">Email is required.</span><span ng-show="registerForm.email.$error.email">Invalid email address.</span><span ng-show="registerForm.email.$error.maxlength">E-mail too long.</span></span></span>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Age -->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="age">Select Basic</label>
-                <div class="col-md-2">
-                    <select id="age" name="age" class="form-control">
-                        <?php echo getAges();?>
-                    </select>
+                <!-- Age -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="age">Age</label>
+                    <div class="col-md-3">
+                        <select id="age" name="age" class="form-control">
+                            <?php echo getAges();?>
+                        </select>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Sex -->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="sex">Sex</label>
-                <div class="col-md-2">
-                    <select id="sex" name="sex" class="form-control">
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                    </select>
+                <!-- Sex -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="sex">Sex</label>
+                    <div class="col-md-3">
+                        <select id="sex" name="sex" class="form-control">
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Country -->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="country">Country</label>
-                <div class="col-md-2">
-                    <select id="country" name="country" class="form-control">
-                        <?php include('html/countries.html'); ?>
-                    </select>
+                <!-- Country -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="country">Country</label>
+                    <div class="col-md-3">
+                        <select id="country" name="country" class="form-control">
+                            <?php include('html/countries.html'); ?>
+                        </select>
+                    </div>
                 </div>
-            </div>
 
-            <!-- City-->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="city">City</label>
-                <div class="col-md-2">
-                    <input id="city" type="text" class="form-control input-md" name="city" ng-model="city" ng-maxlength=20 ng-pattern="/^[a-zA-ZÀ-ž -]*$/" placeholder="City" ng-pattern="[a-zA-Z]" required>
-                    <span class="help-block"><span style="color:red" ng-show="registerForm.city.$dirty && registerForm.city.$invalid"><span ng-show="registerForm.city.$error.required">City is required.</span><span ng-show="registerForm.city.$error.pattern">City must contain only alphabetic letters.</span><span ng-show="registerForm.city.$error.maxlength">City too long.</span></span></span>
+                <!-- City-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="city">City</label>
+                    <div class="col-md-3">
+                        <input id="city" type="text" class="form-control input-md" name="city" ng-model="city" ng-maxlength=20 ng-pattern="/^[a-zA-ZÀ-ž -]*$/" placeholder="City" ng-pattern="[a-zA-Z]" required>
+                        <span class="help-block"><span style="color:red" ng-show="registerForm.city.$dirty && registerForm.city.$invalid"><span ng-show="registerForm.city.$error.required">City is required.</span><span ng-show="registerForm.city.$error.pattern">City must contain only alphabetic letters.</span><span ng-show="registerForm.city.$error.maxlength">City too long.</span></span></span>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Postcode-->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="postcode">Postcode</label>
-                <div class="col-md-2">
-                    <input id="postcode" type="number" class="form-control input-md" name="postcode" ng-model="postcode" ng-maxlength=10 placeholder="Postcode" required >
-                    <span class="help-block"><span style="color:red" ng-show="registerForm.postcode.$dirty && registerForm.postcode.$invalid"><span ng-show="registerForm.postcode.$error.required">Postcode is required.</span><span ng-show="registerForm.postcode.$error.email">Invalid postcode.</span><span ng-show="registerForm.postcode.$error.maxlength">Postcode too long.</span></span></span>
+                <!-- Postcode-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="postcode">Postcode</label>
+                    <div class="col-md-3">
+                        <input id="postcode" type="number" class="form-control input-md" name="postcode" ng-model="postcode" ng-maxlength=10 placeholder="Postcode" required >
+                        <span class="help-block"><span style="color:red" ng-show="registerForm.postcode.$dirty && registerForm.postcode.$invalid"><span ng-show="registerForm.postcode.$error.required">Postcode is required.</span><span ng-show="registerForm.postcode.$error.email">Invalid postcode.</span><span ng-show="registerForm.postcode.$error.maxlength">Postcode too long.</span></span></span>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Captcha-->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="captcha">Captcha</label>
-                <div class="col-md-2">
-                    <input id="captcha" type="text" class="form-control input-md" name="captcha" ng-model="captcha" placeholder="Enter the code below" required>
-                    <span class="help-block"><p><img src="captcha/captcha.php" /></p><span style="color:red" ng-show="registerForm.captcha.$dirty && registerForm.captcha.$invalid"><span ng-show="registerForm.captcha.$error.required">Captcha is required.</span></span></span>
+                <!-- Captcha-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="captcha">Captcha</label>
+                    <div class="col-md-3">
+                        <input id="captcha" type="text" class="form-control input-md" name="captcha" ng-model="captcha" placeholder="Enter the code below" required>
+                        <span class="help-block"><p><img src="captcha/captcha.php" /></p><span style="color:red" ng-show="registerForm.captcha.$dirty && registerForm.captcha.$invalid"><span ng-show="registerForm.captcha.$error.required">Captcha is required.</span></span></span>
+                    </div>
                 </div>
-            </div>
 
-            <!--Submit -->
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="singlebutton"></label>
-                <div class="col-md-2">
-                    <input id="singlebutton" type="submit" class="btn btn-primary" value="Register" ng-disabled="registerForm.name.$invalid || registerForm.surname.$invalid || registerForm.city.$invalid || registerForm.postcode.$invalid ||registerForm.captcha.$invalid || registerForm.email.$invalid">
+                <!--Submit -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="singlebutton"></label>
+                    <div class="col-md-3">
+                        <input id="singlebutton" type="submit" class="btn btn-primary" value="Register" ng-disabled="registerForm.name.$invalid || registerForm.surname.$invalid || registerForm.city.$invalid || registerForm.postcode.$invalid ||registerForm.captcha.$invalid || registerForm.email.$invalid">
+                    </div>
                 </div>
-            </div>
 
-        </fieldset>
-    </form>
+            </fieldset>
+        </form>
+    </div>
 </body>
 </html>
 <?php
--- registerAction.php
+++ registerAction.php
@@ -1,81 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+        session_start();
+        require('connect.php');
+
+        # Post variables
+        $name = $_POST['name'];
+        $surname = $_POST['surname'];
+        $email = $_POST['email'];
+        $age = $_POST['age'];
+        $sex = $_POST['sex'];
+        $country = $_POST['country'];
+        $city = $_POST['city'];
+        $postcode = $_POST['postcode'];
+        $country = $_POST['country'];
+        $captcha = $_POST['captcha'];
+
+        $errors = "";
+
+            #Name variable validation
+            if ($name == NULL || strlen($name) > 20 || !preg_match('/^[a-zA-ZÀ-ž -]*$/',$name)) {
+                $errors .= '<div class="alert alert-danger">* Inserted Name is invalid</div>';
+            }
+
+            #Surname variable validation
+            if ($surname == NULL || strlen($surname) > 20 || !preg_match('/^[a-zA-ZÀ-ž -]*$/',$surname)) {
+                $errors .= '<div class="alert alert-danger">* Inserted Surname is invalid</div>';
+            }
+
+            #E-mail variable validation
+            if ($email == NULL || strlen($email) > 30 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors .= '<div class="alert alert-danger">* Inserted E-mail is invalid</div>';
+            }
+
+            #Ciy variable validation
+            if ($city == NULL || strlen($city) > 20 || !preg_match('/^[a-zA-ZÀ-ž -]*$/',$city)) {
+                $errors .= '<div class="alert alert-danger">* Inserted City is invalid</div>';
+            }
+
+            #Postcode variable validation
+            if ($postcode == NULL || strlen($postcode) > 10 || !preg_match('/^[0-9]*$/',$postcode)) {
+                $errors .= '<div class="alert alert-danger">* Inserted postcode is invalid</div>';
+            }
+
+            #Captcha variable validation
+            if ($captcha != $_SESSION['code']) {
+                $errors .= '<div class="alert alert-danger">* Inserted captcha is invalid</div>';
+            }
+
+            #User registration
+            if ($errors == NULL) {
+                $password = randomPassword();
+                $stmt = $mysqli->prepare("INSERT INTO members(name, surname, email, age, sex, country, city, postcode, password) VALUES (?,?,?,?,?,?,?,?,?)");
+                $stmt->bind_param('sssisssis', $name,
+                    $surname,
+                    $email,
+                    $age,
+                    $sex,
+                    $country,
+                    $city,
+                    $postcode,
+                    $password);
+                if ($stmt->execute()) {
+                    $stmt->close();
+                    echo '<div class="alert alert-success">Registration Successful!</div>';
+                    sendRegistrationEmail($email, $name, $surname, $age, $sex, $country, $city, $postcode, $password);
+                } else {
+                    echo '<div class="alert alert-danger">Registration Failed: Database error</div>';
+                    $stmt->close();
+                }
+            }else{
+                echo '<div class="alert alert-danger">Registration failed due reasons below:</div>'.$errors.'<div class="alert alert-danger"><a href="register.php" class="alert-link">Go back and try again.</a></div>';
+            }
+            ?>
+    </div>
+</body>
+</html>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: IBM-PC
- * Date: 10.12.2014
- * Time: 16:55
- */
-session_start();
-require('connect.php');
-
-# Post variables
-$name = $_POST['name'];
-$surname = $_POST['surname'];
-$email = $_POST['email'];
-$age = $_POST['age'];
-$sex = $_POST['sex'];
-$country = $_POST['country'];
-$city = $_POST['city'];
-$postcode = $_POST['postcode'];
-$country = $_POST['country'];
-$captcha = $_POST['captcha'];
-
-$errors = "";
-
-    #Name variable validation
-    if ($name == NULL || strlen($name) > 20 || !preg_match('/^[a-zA-ZÀ-ž -]*$/',$name)) {
-        $errors .= '<p>* Inserted Name is invalid</p>';
-    }
-
-    #Surname variable validation
-    if ($surname == NULL || strlen($surname) > 20 || !preg_match('/^[a-zA-ZÀ-ž -]*$/',$surname)) {
-        $errors .= '<p>* Inserted Surname is invalid</p>';
-    }
-
-    #E-mail variable validation
-    if ($email == NULL || strlen($email) > 30 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors .= '<p>* Inserted E-mail is invalid</p>';
-    }
-
-    #Ciy variable validation
-    if ($city == NULL || strlen($city) > 20 || !preg_match('/^[a-zA-ZÀ-ž -]*$/',$city)) {
-        $errors .= '<p>* Inserted City is invalid</p>';
-    }
-
-    #Postcode variable validation
-    if ($postcode == NULL || strlen($postcode) > 10 || !preg_match('/^[0-9]*$/',$postcode)) {
-        $errors .= '<p>* Inserted postcode is invalid</p>';
-    }
-
-    #Captcha variable validation
-    if ($captcha != $_SESSION['code']) {
-        $errors .= '<p>* Inserted captcha is invalid</p>';
-    }
-
-    #User registration
-    if ($errors == NULL) {
-        $password = randomPassword();
-        $stmt = $mysqli->prepare("INSERT INTO members(name, surname, email, age, sex, country, city, postcode, password) VALUES (?,?,?,?,?,?,?,?,?)");
-        $stmt->bind_param('sssisssis', $name,
-            $surname,
-            $email,
-            $age,
-            $sex,
-            $country,
-            $city,
-            $postcode,
-            $password);
-        if ($stmt->execute()) {
-            $stmt->close();
-            echo 'Registration Successful!';
-            sendRegistrationEmail($email, $name, $surname, $age, $sex, $country, $city, $postcode, $password);
-        } else {
-            echo 'Registration Failed: Database error';
-            $stmt->close();
-        }
-    }else{
-        echo '<p>Registration failed due reasons below:</p>'.$errors;
-    }
 
     function randomPassword() {
         $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
